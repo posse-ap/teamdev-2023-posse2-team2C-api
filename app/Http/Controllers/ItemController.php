f2db4effa778c3420884eeda3e7e899c2663ba74
karen <caremayo812@icloud.com>
2023-05-22 21:44:12 +0900

    fix: :art: getメソッドに変更＋Auth使った書き方に変更

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\Models\Item;
 use App\Models\Event;
@@ -18,6 +19,12 @@ class ItemController extends Controller
         $status = $item->status_id;
         $price = $status === 1 ? "???" : $item->price;
         $created_at = (new Carbon($item->created_at))->toDateString();
+        if (Auth::check()) {
+            $user_point = Auth::user()->point;
+        }else{
+            $user_point = null;
+
+        }
 
         return [
             "id" => $item->id,
@@ -30,7 +37,8 @@ class ItemController extends Controller
             "status" => $status,
             "price" => $price,
             "created_at" => $created_at,
-            "history" => $item->history()
+            "history" => $item->history(),
+            "user_point" => $user_point
         ];
     }
 
@@ -58,37 +66,22 @@ class ItemController extends Controller
     // レンタル時のPOSTまとめ
     // ①web.phpにメソッド追加 Route::post('/items/rental/{user_id}', [ItemController::class, 'storeRentalData']);
     // ②テーブルに挿入
-    public function storeRentalData($item_id, $request){
+    public function storeRentalData($item_id){
         $rental = new Rental;
         $rental->item_id = $item_id;
-        $rental->user_id = $request->user_id;
-        $rental->owner_id = Item::shownCards()->find($item_id)->owner();
+        $rental->user_id = Auth::id();
+        $rental->owner_id = Item::shownCards()->find($item_id)->owner_id;
         $rental->save();
 
         $parent_id = $rental->id; // 親テーブルのIDを取得
 
         $rental_history = new Rental_points_withdraw_history;
-        $rental_history->user_id = $request->user_id;
+        $rental_history->user_id = Auth::id();
         $rental_history->amount = Item::shownCards()->find($item_id)->price;
         $rental_history->rental_id = $parent_id;
         $rental_history->type = 1;
         $rental_history->save();
+
+        return response()->json('レンタル完了', 200);
     }
-
-    /* rentalsテーブルに以下の情報を挿入する
-    id	int(auto)	
-    item_id	int	（もらってくる）
-    user_id	int	（もらってくる）
-    owner_id	int	（itemのownerからもらってくる）
-    created_at	date	
-    deleted_at	date
-
-    rental_points_withdraw_history	
-    id	int(auto)	
-    user_id	int	（済）
-    amount	int	　（itemのpriceからもらってくる）
-    rental_id	int	　（itemのidと同値）
-    type	int	（新規なので1！）
-    created_at	date
-    */
 }

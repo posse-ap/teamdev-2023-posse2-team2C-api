7dabd71368dd1deb9c7a4b7c89d02fbe73455f43
watanabe-miyuki <kirin.myk2018@gmail.com>
2023-05-27 11:02:11 +0900

    feat: :art: history model に Rental リレーション追加

--- app/Models/Rental_points_withdraw_history.php
+++ app/Models/Rental_points_withdraw_history.php
@@ -4,17 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use App\Models\Rental;
 
 class Rental_points_withdraw_history extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = [];
 
     // 対応するレンタルを呼び出し
-    public function rental () {
-        return $this -> belongsTo(Rental::class, 'rental_id', 'id')->first();
+    public function rental()
+    {
+        return $this->belongsTo(Rental::class, 'rental_id', 'id')->first();
+    }
+
+    public function rental_relation()
+    {
+        return $this->belongsTo(Rental::class, 'rental_id', 'id');
     }
 }

cbeb9a01a3489cd42f31a7dd30dbe9075afa5b36
watanabe-miyuki <kirin.myk2018@gmail.com>
2023-05-28 12:51:17 +0900

    refactor: :fire: use Exception　削除

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-use Exception;
 use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
